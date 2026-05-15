1c0bd9df53e73c9f967dcea7a35ba9f08df0518b
isabelmarx <imarxv@gmail.com>
2026-05-15 19:31:50 -0400

    flake8 compliance

--- examples/fetch_abstracts.py
+++ examples/fetch_abstracts.py
@@ -1,6 +1,6 @@
 from pubmedflow import LazyPubmed
-pb        = LazyPubmed()
+pb = LazyPubmed()
 
-result    = pb.fetch(query = "lncRNA",
-                    key = "your_api_key", 
-                    max_documents = 5)
+result = pb.fetch(query="lncRNA",
+                  key="your_api_key",
+                  max_documents=5)
